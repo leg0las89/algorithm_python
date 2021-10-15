0670fba2c4f31afc6d2577d8d8c1cbe02076a575
Viktor <leg0las8930@gmail.com>
2021-10-14 21:55:00 -0400

    asd

--- greater_common_divisor.py
+++ greater_common_divisor.py
@@ -1,7 +1,7 @@
 #How to find GCD (Greatest Common Divisor) of two numbers using recursion
 
 def gcd(a,b):
-    print(a,b)
+    assert int(a) == a and int(b) == b , "Use integer nuber"
     if a < 0:
         a = a * -1
     if b < 0:
